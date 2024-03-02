b2c9ef265e570ac0b5ee9446c6873ea119d5a49b
Maria Vasylets <mashkavasylers@gmail.com>
2024-03-02 15:48:49 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 4;
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        int c = 45;
+        System.out.println(c/10+c%10);
+
+        double d = 4.5;
+        System.out.println((int)(d+0.5));
+
+    }
+}
